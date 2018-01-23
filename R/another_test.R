9f95f477c1dbfd0dd7e65233ec35b2a4641e1a7c
CasAndreu <acasas2@uw.edu>
2018-01-23 13:21:20 -0500

    new test function

---
+++ R/another_test.R
@@ -0,0 +1,12 @@
+#' Another test function
+#'
+#' This function is a test.
+#' @param word This is word will be printed in the terminal
+#' @keywords another test
+#' @export
+#' @examples
+#' another_test_function(word)
+
+another_test_function <- function(word = "test"){
+  print(paste0("Hello ", word))
+}
---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' Test function
+#'
+#' This function is a test.
+#' @param word This is word will be printed in the terminal
+#' @keywords test
+#' @export
+#' @examples
+#' test_function(word)
+
+test_function <- function(word = "test"){
+  print(paste0("Hello ", word))
+}
